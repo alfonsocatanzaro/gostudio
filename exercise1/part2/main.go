8938a6864e935a67b4b4ace65c259620e23875b8
Alfonso Catanzaro [eADE] <a.catanzaro@eade.it>
2019-02-25 14:25:50 +0100

    Done

--- exercise1/part2/main.go
+++ exercise1/part2/main.go
@@ -13,7 +13,6 @@ func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format 'question,answer'")
 	timeLimitInSecond := flag.Duration("s", 30, "time limit in seconds.")
 	flag.Parse()
-	tmr := time.NewTimer(timeLimitInSecond)
 
 	file, err := os.Open(*csvFilename)
 	if err != nil {
@@ -29,17 +28,26 @@ func main() {
 
 	fmt.Printf("Premi invio per iniziare il quiz!")
 	fmt.Scanf("\n")
-
-	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+	tmr := time.NewTimer(*timeLimitInSecond)
+	endQuiz := make(chan bool)
+	go func() {
+		for i, p := range problems {
+			fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			if answer == p.a {
+				correct++
+			}
 		}
-	}
+		endQuiz <- true
+	}()
 
-	fmt.Printf("%d risposte corrette su %d\n", correct, len(problems))
+	select {
+	case <-tmr.C:
+		fmt.Printf("Tempo scaduto")
+	case <-endQuiz:
+		fmt.Printf("%d risposte corrette su %d\n", correct, len(problems))
+	}
 
 }
 
