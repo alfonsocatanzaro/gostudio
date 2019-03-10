c3be9d987b5841e33a43e39d5a3bc4183ced6359
Alfonso Catanzaro [Casa] <alfonso_catanzaro@hotmail.com>
2019-03-10 23:02:44 +0100

    httphandler

--- exercise3/cyoa/cmd/cyoaweb/main.go
+++ exercise3/cyoa/cmd/cyoaweb/main.go
@@ -4,10 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"gostudio/exercise3/cyoa"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "The port to start the CYOA web application on")
 	filename := flag.String("file", "gopher.json", "The JSON file with the CYOA story")
 	flag.Parse()
 
@@ -21,5 +24,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("starting the server at: %d\n", *port)
+	addr := fmt.Sprintf(":%d", *port)
+	log.Fatal(http.ListenAndServe(addr, h))
 }
