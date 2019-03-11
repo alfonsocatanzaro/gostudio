afdfd77f02858b496506b9b32f9459a18353ce94
Alfonso Catanzaro [D260] <alfonso.catanzaro@gmail.com>
2019-03-11 20:59:20 +0100

    Custom template (incomplete)

--- exercise3/cyoa/cmd/cyoaweb/main.go
+++ exercise3/cyoa/cmd/cyoaweb/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"gostudio/exercise3/cyoa"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -12,9 +13,11 @@ import (
 func main() {
 	port := flag.Int("port", 3000, "The port to start the CYOA web application on")
 	filename := flag.String("file", "gopher.json", "The JSON file with the CYOA story")
+	templ := flag.String("template", "", "HTML Template to use")
 	flag.Parse()
 
 	fmt.Printf("Using the story in %s.\n", *filename)
+	fmt.Printf("Using the template in %s.\n", *templ)
 	f, err := os.Open(*filename)
 	if err != nil {
 		panic(err)
@@ -24,7 +27,15 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	h := cyoa.NewHandler(story)
+
+	var t *template.Template
+
+	if *templ != "" {
+		fmt.Printf("eccolo %s.\n", *templ)
+		t = template.Must(template.New("").ParseFiles(*templ))
+	}
+
+	h := cyoa.NewHandler(story, t)
 	fmt.Printf("starting the server at: %d\n", *port)
 	addr := fmt.Sprintf(":%d", *port)
 	log.Fatal(http.ListenAndServe(addr, h))
--- exercise3/cyoa/story.go
+++ exercise3/cyoa/story.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func init() {
@@ -13,41 +15,106 @@ func init() {
 
 var tpl *template.Template
 
-var defaultHandlerTmpl = `
-<!DOCTYPE html>
+var defaultHandlerTmpl = `<!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8">
-    <title>Choose You Own Adventure</title>
+  <meta charset="utf-8">
+  <title>Choose You Own Adventure</title>
 </head>
+
 <body>
+  <section class="page">
     <h1>{{.Title}}</h1>
     {{range .Paragraphs}}
     <p>{{.}}</p>
     {{end}}
     <ul>
-        {{range .Options}}
-        <li><a href="/{{.Arc}}">{{.Text}}</a></li>
-        {{end}}
+      {{range .Options}}
+      <li><a href="/{{.Arc}}">{{.Text}}</a></li>
+      {{end}}
     </ul>
+  </section>
+  <style>
+    body {
+      font-family: helvetica, arial;
+    }
+
+    h1 {
+      text-align: center;
+      position: relative;
+    }
+
+    .page {
+      width: 80%;
+      max-width: 500px;
+      margin: auto;
+      margin-top: 40px;
+      margin-bottom: 40px;
+      padding: 80px;
+      background: #FFFCF6;
+      border: 1px solid #eee;
+      box-shadow: 0 10px 6px -6px #777;
+    }
+
+    ul {
+      border-top: 1px dotted #ccc;
+      padding: 10px 0 0 0;
+      -webkit-padding-start: 0;
+    }
+
+    li {
+      padding-top: 10px;
+    }
+
+    a,
+    a:visited {
+      text-decoration: none;
+      color: #6295b5;
+    }
+
+    a:active,
+    a:hover {
+      color: #7792a2;
+    }
+
+    p {
+      text-indent: 1em;
+    }
+  </style>
 </body>
-</html>
-`
+
+</html>`
 
 // NewHandler create a handler object to serve
-func NewHandler(s Story) http.Handler {
-	return handler{s}
+func NewHandler(s Story, t *template.Template) http.Handler {
+	if t == nil {
+		t = tpl
+
+	}
+	return handler{s, t}
 }
 
 type handler struct {
 	s Story
+	t *template.Template
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := tpl.Execute(w, h.s["intro"])
-	if err != nil {
-		panic(err)
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
 	}
+	path = path[1:]
+	if chapter, ok := h.s[path]; ok {
+		err := h.t.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "Chapter not found", http.StatusNotFound)
 
 }
 
