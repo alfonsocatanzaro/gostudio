9f66b9196f318943ea9a156003bcbb422e5c3dc0
Alfonso Catanzaro [Casa] <alfonso_catanzaro@hotmail.com>
2019-03-20 23:03:31 +0100

    Exercise 5 - Parsing and Building Links

--- exercise5/main.go
+++ exercise5/main.go
@@ -3,20 +3,61 @@ package main
 import (
 	"flag"
 	"fmt"
+	"gostudio/exercise4/link"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
+/*
+	1. GET the webage
+	2. parse all the links on the page
+	3. build proper urls with our links
+	4. filter out any links w/ a diff domain
+	5. Find all pages (BFS)
+	6. print out XML
+*/
+
 func main() {
-	urlFlag := flag.String("url", "https://gophercises.com", "Url of the site to build sitemat from.")
+	urlFlag := flag.String("url", "https://gophercises.com/", "Url of the site to build sitemat from.")
 	flag.Parse()
 
-	fmt.Println(*urlFlag)
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	reqURL := resp.Request.URL
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme,
+		Host:   reqURL.Host,
+	}
+
+	base := baseURL.String()
+
+	links, _ := link.Parse(resp.Body)
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+
+	for _, l := range hrefs {
+		fmt.Println(l)
+	}
 
 	/*
-		1. GET the webage
-		2. parse all the links on the page
-		3. build proper urls with our links
-		4. filter out any links w/ a diff domain
-		5. Find all pages (BFS)
-		6. print out XML
+		/some-path
+		https://gophercises.com/some-path
+		http://gophercises.com/some-path
+		#fragments
+		mailto:mail@domain.com
 	*/
+
 }
