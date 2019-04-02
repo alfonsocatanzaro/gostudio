ceb07e0389ddab55d101720b507bc4189ab8117b
Alfonso Catanzaro [eADE] <a.catanzaro@eade.it>
2019-04-02 10:25:32 +0200

    cipher start

--- exercise6/cipher/cipher.go
+++ exercise6/cipher/cipher.go
@@ -2,29 +2,43 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	var input string
-	fmt.Scanf("%s\n", &input)
-	//output := ""
-	n1 := 10
-	n2 := n1 << 2
-	n3 := n2 << 2
-	fmt.Printf("%b \t %d \n ", n1, n1)
-	fmt.Printf("%b \t %d \n ", n2, n2)
-	fmt.Printf("%b \t %d \n ", n3, n3)
-	// scanner := bufio.NewScanner(os.Stdin)
+	var length int
+	var shiftBy int32
+	var input1, input2, input3 string
 
-	// for scanner.Scan() {
-	// 	word := scanner.Text()
-	// 	n := 1
-	// 	for i := 0; i < len(word); i++ {
-	// 		if word[i] >= 'A' && word[i] <= 'Z' {
-	// 			n++
-	// 		}
-	// 	}
-	// 	fmt.Println(n)
-	// }
+	fmt.Scanf("%s\n", &input1)
+	if n, err := strconv.Atoi(input1); err == nil {
+		length = int(n)
+	}
+	fmt.Scanf("%s\n", &input2)
 
+	fmt.Scanf("%s\n", &input3)
+	if n, err := strconv.Atoi(input3); err == nil {
+		shiftBy = int32(n)
+	}
+
+	const minLower, maxLower, minUPPER, maxUPPER = 'a', 'z', 'A', 'Z'
+	var output string
+	for i := 0; i < length; i++ {
+		ch := int32(input2[i])
+		if ch >= minLower && ch <= maxLower {
+			ch += shiftBy
+			if ch > maxLower {
+				ch = ch + minLower - maxLower - 1
+			}
+		}
+		if ch >= minUPPER && ch <= maxUPPER {
+			ch += shiftBy
+			if ch > maxUPPER {
+				ch = ch + minUPPER - maxUPPER - 1
+			}
+		}
+		output += string(ch)
+	}
+	fmt.Println(input2)
+	fmt.Println(output)
 }
