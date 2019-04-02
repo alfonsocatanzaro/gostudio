a91f9ee15168343760db1eaea7d1ef479ba65503
Alfonso Catanzaro [D260] <alfonso.catanzaro@gmail.com>
2019-04-02 23:11:40 +0200

    exercise 6 Done

--- exercise6/cipher/cipher.go
+++ exercise6/cipher/cipher.go
@@ -2,43 +2,38 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
 	var length int
-	var shiftBy int32
-	var input1, input2, input3 string
+	var input string
+	var delta int
 
-	fmt.Scanf("%s\n", &input1)
-	if n, err := strconv.Atoi(input1); err == nil {
-		length = int(n)
-	}
-	fmt.Scanf("%s\n", &input2)
+	fmt.Scanf("%d\n", &length)
+	fmt.Scanf("%s\n", &input)
+	fmt.Scanf("%d\n", &delta)
 
-	fmt.Scanf("%s\n", &input3)
-	if n, err := strconv.Atoi(input3); err == nil {
-		shiftBy = int32(n)
+	var ret []rune
+	for _, ch := range input {
+		ret = append(ret, chipher(ch, delta))
 	}
 
-	const minLower, maxLower, minUPPER, maxUPPER = 'a', 'z', 'A', 'Z'
-	var output string
-	for i := 0; i < length; i++ {
-		ch := int32(input2[i])
-		if ch >= minLower && ch <= maxLower {
-			ch += shiftBy
-			if ch > maxLower {
-				ch = ch + minLower - maxLower - 1
-			}
-		}
-		if ch >= minUPPER && ch <= maxUPPER {
-			ch += shiftBy
-			if ch > maxUPPER {
-				ch = ch + minUPPER - maxUPPER - 1
-			}
-		}
-		output += string(ch)
+	fmt.Println(string(ret))
+
+}
+
+func chipher(r rune, delta int) rune {
+	if r >= 'A' && r <= 'Z' {
+		return rotate(r, 'A', delta)
 	}
-	fmt.Println(input2)
-	fmt.Println(output)
+	if r >= 'a' && r <= 'z' {
+		return rotate(r, 'a', delta)
+	}
+	return r
+}
+
+func rotate(r rune, base, delta int) rune {
+	tmp := int(r) - base
+	tmp = (tmp + delta) % 26
+	return rune(tmp + base)
 }
