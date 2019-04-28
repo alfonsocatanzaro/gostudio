42e48ccba2e2bbbbd13a6e01f4389260761b2038
Alfonso On HP <alfonso.catanzaro@gmail.com>
2019-04-28 22:37:04 +0200

    Creating a Bucket

---
+++ task/db/tasks.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/boltdb/bolt"
+	"log"
+	"time"
+)
+
+func main(){
+	db, err := bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+
+}
+
