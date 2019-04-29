50aa8862e13e5bf0d7cb1272a2b90cf7ff41d633
Alfonso On HP <alfonso.catanzaro@gmail.com>
2019-04-29 23:00:55 +0200

    finalizing task list

--- task/cmd/list.go
+++ task/cmd/list.go
@@ -2,8 +2,9 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/gostudio/task/db"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 // listCmd represents the list command
@@ -11,7 +12,21 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all of your tasks.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) ==0 {
+			fmt.Println("You have no tasks to complete! Why not take a vacation? 🏖")
+			return
+		}
+		fmt.Printf("You have the following tasks:")
+		for i,task := range tasks{
+			fmt.Printf("%d. %s\n",i+1,task.Value)
+			fmt.Printf("%d\n", task.Key)
+		}
+
 	},
 }
 
