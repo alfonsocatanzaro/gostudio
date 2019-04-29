23147881af40b6c862fea2b0d3e68e2e65ce365c
Alfonso Catanzaro [eADE] <a.catanzaro@eade.it>
2019-04-29 10:05:52 +0200

    Storing tasks

--- task/db/tasks.go
+++ task/db/tasks.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"encoding/binary"
+	// go get -u github.com/boltdb/bolt/...
 	"github.com/boltdb/bolt"
 	"time"
 )
@@ -23,4 +25,29 @@ func Init(dbPath string) error {
 		_, err := tx.CreateBucketIfNotExists(tasksBucket)
 		return err
 	})
+}
+
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(tasksBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key:=iTob(int(id64))
+		return b.Put(key, []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func iTob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
+func btoi (b []byte) int{
+	return int(binary.BigEndian.Uint64(b))
 }
